db6a5f22d4594983d96b7ca64237641301f0c994
DUNG30 <thiidun3460@gmail.com>
2025-11-07 20:41:12 +0700

    update code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,18 +8,22 @@
 <body>
 <div class="container mt-5">
     <h2>Admin Login</h2>
-    <form action="{{ route('admin.login') }}" method="POST">
+
+    <form action="{{ route('admin.login.post') }}" method="POST">
         @csrf
         <div class="mb-3">
             <label>Email</label>
             <input type="email" name="email" class="form-control" required>
         </div>
+
         <div class="mb-3">
             <label>Password</label>
             <input type="password" name="password" class="form-control" required>
         </div>
+
         <button class="btn btn-primary">Login</button>
     </form>
+
 </div>
 </body>
 </html>
